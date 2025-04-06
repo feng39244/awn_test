9e0e586f901631d9fe655c81c3f96feffbe25416
jifeng qian <feng39244@gmail.com>
2025-04-06 08:56:22 -0700

    update classification of the 1500 fields

--- bill/fill_1500_massage_template.py
+++ bill/fill_1500_massage_template.py
@@ -21,6 +21,7 @@ doc_fields = {
     'doc_location': 'Corona CA 92879',
     'doc_phone area': '951',
     'doc_phone': '371-8888',
+    'tax_id': '45-0941589',
 }
 
 patient_fields = {
@@ -33,7 +34,9 @@ patient_fields = {
     'pt_state': 'CA',
     'pt_zip': '90001',
     'pt_AreaCode': '310',
-    'pt_phone': '555-1234'
+    'pt_phone': '555-1234',
+    'pt_signature': 'ON FILE',
+    'pt_date': 'Not provided',
 }
 
 insurance_fields = {
@@ -60,12 +63,51 @@ insurer_fields = {
     'other_ins_name': 'Not provided',
     'other_ins_policy': 'Not provided',
     'other_ins_plan_name': 'Not provided',
+    'ins_signature': 'ON FILE',
+}
+
+
+claim_fields = {
+    'cur_ill_mm': 'Not provided',
+    'cur_ill_dd': 'Not provided',
+    'cur_ill_yy': 'Not provided',
+    'ref_physician': 'Not provided',
+    'id_physician': 'Not provided',
+    '96': 'Not provided', # additional claim information
+    '99icd': 'Not provided',  #icd 9 ind e
+    'diagnosis1': 'Not provided',
+    'diagnosis2': 'Not provided',
+    'diagnosis3': 'Not provided',
+    'diagnosis4': 'Not provided',
+    'diagnosis5': 'Not provided',
+    'diagnosis6': 'Not provided',
+    'diagnosis7': 'Not provided',
+    'diagnosis8': 'Not provided',
+    'diagnosis9': 'Not provided',
+    'diagnosis10': 'Not provided',
+    'diagnosis11': 'Not provided',
+    'diagnosis12': 'Not provided',
+    'prior_auth': 'Not provided',
+
+}
+
+massage_treatment_therapist_fields = {
+    'local1a': 'Not provided', # therapist SSN
+    'local1': 'Not provided', # therapist NPI
+    'loca2a': 'Not provided', # therapist SSN
+    'local2': 'Not provided', # therapist NPI
+    'local3a': 'Not provided', # therapist SSN
+    'local3': 'Not provided', # therapist NPI
+    'local4a': 'Not provided', # therapist SSN
+    'local4': 'Not provided', # therapist NPI
+    'local5a': 'Not provided', # therapist SSN
+    'local5': 'Not provided', # therapist NPI
+    'local6a': 'Not provided', # therapist SSN
+    'local6': 'Not provided', # therapist NPI
 }
 
 massage_treatment_fields = {
     'emg1': 'Not provided',
-    'local1a': 'Not provided',
-    'local1': 'Not provided',
     'sv1_mm_from': 'Not provided',
     'sv1_dd_from': 'Not provided',
     'sv1_yy_from': 'Not provided',
@@ -86,7 +128,6 @@ massage_treatment_fields = {
     'epsdt1': 'Not provided',
     'Suppla': 'Not provided',
     'emg2': 'Not provided',
-    'local2a': 'Not provided',
     'sv2_mm_from': 'Not provided',
     'sv2_dd_from': 'Not provided',
     'sv2_yy_from': 'Not provided',
@@ -105,10 +146,8 @@ massage_treatment_fields = {
     '157': 'Not provided',  # Unclear field reference
     'day2': 'Not provided',
     'plan2': 'Not provided',
-    'local2': 'Not provided',
     'Supplb': 'Not provided',
     'emg3': 'Not provided',
-    'local3a': 'Not provided',
     'sv3_mm_from': 'Not provided',
     'sv3_dd_from': 'Not provided',
     'sv3_yy_from': 'Not provided',
@@ -127,10 +166,8 @@ massage_treatment_fields = {
     '179': 'Not provided',  # Unclear field reference
     'day3': 'Not provided',
     'plan3': 'Not provided',
-    'local3': 'Not provided',
     'Supplc': 'Not provided',
     'emg4': 'Not provided',
-    'local4a': 'Not provided',
     'sv4_mm_from': 'Not provided',
     'sv4_dd_from': 'Not provided',
     'sv4_yy_from': 'Not provided',
@@ -149,10 +186,8 @@ massage_treatment_fields = {
     '201': 'Not provided',  # Unclear field reference
     'day4': 'Not provided',
     'plan4': 'Not provided',
-    'local4': 'Not provided',
     'Suppld': 'Not provided',
     'emg5': 'Not provided',
-    'local5a': 'Not provided',
     'sv5_mm_from': 'Not provided',
     'sv5_dd_from': 'Not provided',
     'sv5_yy_from': 'Not provided',
@@ -171,10 +206,8 @@ massage_treatment_fields = {
     '223': 'Not provided',  # Unclear field reference
     'day5': 'Not provided',
     'plan5': 'Not provided',
-    'local5': 'Not provided',
     'Supple': 'Not provided',
     'emg6': 'Not provided',
-    'local6a': 'Not provided',
     'sv6_mm_from': 'Not provided',
     'sv6_dd_from': 'Not provided',
     'sv6_yy_from': 'Not provided',
@@ -199,22 +232,12 @@ massage_treatment_fields = {
 
 massage_fields = {
     'NUCC USE': 'Not provided',
-    
-    
-    
     '40': 'Not provided',  # Unclear field reference
     'accident_place': 'Not provided',
     '57': 'Not provided',  # Unclear field reference
     '58': 'Not provided',  # Unclear field reference
-    '41': 'Not provided',  # Unclear field reference
-    
+    '41': 'Not provided',  # Unclear field reference 
     '50': 'Not provided',  # Unclear field reference
-    'pt_signature': 'Not provided',
-    'pt_date': 'Not provided',
-    'ins_signature': 'Not provided',
-    'cur_ill_mm': 'Not provided',
-    'cur_ill_dd': 'Not provided',
-    'cur_ill_yy': 'Not provided',
     '73': 'Not provided',  # Unclear field reference
     '74': 'Not provided',  # Unclear field reference
     'sim_ill_mm': 'Not provided',
@@ -229,39 +252,18 @@ massage_fields = {
     'physician number 17a1': 'Not provided',
     'physician number 17a': 'Not provided',
     '85': 'Not provided',  # Unclear field reference
-    'ref_physician': 'Not provided',
-    'id_physician': 'Not provided',
     'hosp_mm_from': 'Not provided',
     'hosp_dd_from': 'Not provided',
     'hosp_yy_from': 'Not provided',
     'hosp_mm_end': 'Not provided',
     'hosp_dd_end': 'Not provided',
     'hosp_yy_end': 'Not provided',
-    '96': 'Not provided',  # Unclear field reference
     'charge': 'Not provided',
-    '99icd': 'Not provided',  # Likely a typo for ICD-9/ICD-10 codes
-    'diagnosis1': 'Not provided',
-    'diagnosis2': 'Not provided',
-    'diagnosis3': 'Not provided',
-    'diagnosis4': 'Not provided',
     'medicaid_resub': 'Not provided',
     'original_ref': 'Not provided',
-    'diagnosis5': 'Not provided',
-    'diagnosis6': 'Not provided',
-    'diagnosis7': 'Not provided',
-    'diagnosis8': 'Not provided',
-    'diagnosis9': 'Not provided',
-    'diagnosis10': 'Not provided',
-    'diagnosis11': 'Not provided',
-    'diagnosis12': 'Not provided',
-    'prior_auth': 'Not provided',
     'Suppl': 'Not provided',
-    
-    
-    
-    
     '276': 'Not provided',  # Unclear field reference
-    'tax_id': 'Not provided',
+    
     'pt_account': 'Not provided',
     't_charge': 'Not provided',
     'amt_paid': 'Not provided',
